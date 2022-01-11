2c14a48ca3ed6ab7d60c4ccb119116d66f7a5428
Sporech <edwinclarke4@gmail.com>
2022-01-11 11:23:49 +0000

    Start of term, issues still being sorted, changing coords_to_instructions after this commit

--- ASMBL_Processing.py
+++ ASMBL_Processing.py
@@ -1,18 +1,31 @@
 # Copyright (c) 2017 Ruben Dulek
 # The PostProcessingPlugin is released under the terms of the AGPLv3 or higher.
 
+from io import BufferedRandom
 import re #To perform the search and replace.
 import traceback
 import sys
+
+from shapely.geometry import multipolygon
 
 from ..Script import Script
 from UM.Message import Message
 from UM.Logger import Logger
 
+from UM.Application import Application
+
 from shapely import geometry
+from shapely.ops import unary_union
 
 import numpy as np
-#import matplotlib.pyplot as plt
+
+try:
+    import matplotlib.pyplot as plt
+    import geopandas as gpd
+except:
+    Message(traceback.format_exc(), title = "Exception").show()
+
+debug_layer_count = 0
 
 class MoveInstruction:
     def __init__(self, instruction):
@@ -28,56 +41,119 @@ class MoveInstruction:
             elif "F" in arg:
                 self.f = float(arg.split("F")[1])
 
-
 class Layer:
 
     def __init__(self, layer_instructions):
+        self.polygons = []
         self.extractGCodeLayer(layer_instructions)
+        self.original_gcode = layer_instructions.splitlines()
 
     def extractGCodeLayer(self, gcode):
-        move_instructions = []
+        gcode_lines = gcode.splitlines()
 
-        try:
-            start = ";TYPE:WALL-OUTER"
-            end = ";MESH:NONMESH"
-            wall_gcode = gcode[gcode.find(start)+len(start):gcode.rfind(end)]
-            #wall_gcode = re.search(';TYPE:WALL-OUTER(.*);MESH:NONMESH', gcode).group(1)
-        except:
-            return
+        while ";TYPE:WALL-OUTER" in gcode_lines:
+            move_instructions = []
+            start_index = gcode_lines.index(";TYPE:WALL-OUTER")
+            for i in range(start_index+1,len(gcode_lines)-1):
+                if i == -1:
+                    break
+                if not "E" in gcode_lines[i]:
+                    break
+                if not "X" in gcode_lines[i] and not "Y" in gcode_lines[i]:
+                    continue
+                move_instructions.append(MoveInstruction(gcode_lines[i]))
 
-        #Message(gcode, title = "Parsed GCode").show()
-        for line in wall_gcode.splitlines():
-            if len(line) <= 0:
-                continue
-            if (line[0] == "G") and (("X" in line) or ("Y" in line)):
-                move_instructions.append(MoveInstruction(line))
-
-        p = lambda arr: [(instruction.x, instruction.y) for instruction in arr]
-
-        self.polygon = geometry.Polygon(p(move_instructions))
-
-        #x,y = self.polygon.exterior.xy
-        #plt.plot(x,y)
-
-    def expand(self, distance):
-
-        try:
-            buffered_poly = self.polygon.buffer(distance)
+            p = lambda arr: [(instruction.x, instruction.y) for instruction in arr]
+            self.polygons.append(geometry.Polygon(p(move_instructions)))
             
-            x_coords,y_coords = buffered_poly.exterior.xy
-            x_coords = list(x_coords)
-            y_coords = list(y_coords)
+            del gcode_lines[start_index]
 
-            instructions = []
+    def coords_to_instructions(self,shapes, extrude = True):
+        x_coords = []
+        y_coords = []
+        for poly in shapes:
+            if not hasattr(poly,'exterior'):
+                Logger.log("d","No exterior found")
+                continue
+            xs,ys = poly.exterior.xy
+            x_coords.append(list(xs))
+            y_coords.append(list(ys))
 
-            for i in range(len(x_coords)-1):
-                ins = "G1 X" + str(x_coords[i]) + " Y" + str(y_coords[i]) + "\n"
+        instructions = []
+        for i in range(len(x_coords)-1):
+            for j in range(len(x_coords[i])-1):
+
+                if extrude and j > 0:
+                    E = 0
+
+                    layerHeight = Application.getInstance().getGlobalContainerStack().getProperty("layer_height", "value")
+                    extruderDiameter = Application.getInstance().getGlobalContainerStack().getProperty("machine_nozzle_size", "value")
+
+                    distance = ((x_coords[i][j]-x_coords[i][j-1])**2 + (y_coords[i][j]-y_coords[i][j-1])**2)**0.5
+
+                    E = (4 * layerHeight * distance) / (3.1415 * extruderDiameter) #SOURCE https://3dprinting.stackexchange.com/questions/6289/how-is-the-e-argument-calculated-for-a-given-g1-command
+
+                    ins = "G1 X" + str(x_coords[i][j]) + " Y" + str(y_coords[i][j]) + " E" + str(E) + "\n"
+                else:
+                    ins = "G1 X" + str(x_coords[i][j]) + " Y" + str(y_coords[i][j]) + "\n"
                 instructions.append(ins)
 
-            return instructions
+            #INSERT TRAVEL HERE?
 
-        except:
-            return ""
+        #Logger.log("d","Number of coordinates = "+str(len(instructions))) Not there yet
+        Logger.log("d","Number of instructions = "+str(len(instructions)))
+        Logger.log("d","Instructions (return of coords_to_instructions) = "+str(instructions))
+
+
+        return instructions
+
+    def expand(self, distance):
+        global debug_layer_count
+
+        additive_polys = []
+        subtractive_polys = []
+
+        lineWidth = Application.getInstance().getGlobalContainerStack().getProperty("line_width", "value")
+
+        for polygon in self.polygons:
+            subtractive_polys.append(polygon.buffer(distance))
+            additive_polys.append(polygon.buffer(lineWidth))
+
+        #DEBUGGING!
+        if debug:
+            debug_layer_count += 1
+
+            if debug_layer_count == debug_layer:
+                try:
+                    fig, (axs1,axs2) = plt.subplots(1,2)
+
+                    axs1.title.set_text("Polygon Original")
+                    axs2.title.set_text("Polygon Buffered")
+
+                    #axs1.title.set_x("mm")
+                    #axs1.title.set_y("mm")
+
+                    #axs2.title.set_x("mm")
+                    #axs2.title.set_y("mm")
+
+                    p = gpd.GeoSeries(unary_union(self.polygons))
+                    p.plot(ax=axs1)
+                    plt.show()
+                    p = gpd.GeoSeries(unary_union(subtractive_polys))
+                    p.plot(ax=axs2)
+                    plt.show()
+                except:
+                    Message(traceback.format_exc(), title = "Plot Exception").show()
+        #END OF DEBUGGING
+
+        add_instructions = self.coords_to_instructions(additive_polys)
+        sub_instructions = self.coords_to_instructions(subtractive_polys,extrude=False)
+
+        #Logger.log("d","Number of coordinates = "+str(len(instructions))) Not there yet
+        #Logger.log("d","Number of sub instructions = "+str(len(sub_instructions)))
+        #Logger.log("d","Sub Instructions (return of expand) = "+str(sub_instructions))
+
+        return add_instructions,sub_instructions
 
 
 class ASMBL_Processing(Script):
@@ -95,6 +171,13 @@ class ASMBL_Processing(Script):
             "version": 2,
             "settings":
             {
+                "SubtractiveEnabled":
+                {
+                    "label": "Subtractive Tool Enabled",
+                    "description": "Enables or disables the subtractive tool",
+                    "type": "bool",
+                    "default_value": "True"
+                },
                 "SubtractivePerimeter":
                 {
                     "label": "Subtractive Tool Perimeter",
@@ -102,44 +185,84 @@ class ASMBL_Processing(Script):
                     "type": "float",
                     "default_value": "3.5"
                 },
-                "ToSub":
+                "SubtractiveFeedrate":
                 {
-                    "label": "Subtractive Tool Macro",
-                    "description": "Macro file to remap to tool 2",
-                    "type": "str",
-                    "default_value": "idex_remap_tool2.gcode"
+                    "label": "Subtractive Feed Rate",
+                    "description": "Feedrate for the Subtractive Tool",
+                    "type": "int",
+                    "default_value": "3000"
                 },
-                "ToAdd":
+                "AdditiveEnabled":
                 {
-                    "label": "Additive Tool Macro",
-                    "description": "Macro file to remap to tool 1",
-                    "type": "str",
-                    "default_value": "idex_remap_tool1.gcode"
+                    "label": "Extra Additive Layer Enabled",
+                    "description": "Enables or disables the additional additive layer",
+                    "type": "bool",
+                    "default_value": "False"
+                },
+                "AdditiveFeedrate":
+                {
+                    "label": "Additive Feed Rate",
+                    "description": "Feedrate for the Extra Wall",
+                    "type": "int",
+                    "default_value": "3000"
+                },
+                "DebugEnabled":
+                {
+                    "label": "Debug Mode",
+                    "description": "Is debug mode on?",
+                    "type": "bool",
+                    "default_value": "False"
+                },
+                "DebugLayer":
+                {
+                    "label": "Visualised Layer Number",
+                    "description": "Which layer the debugger should display",
+                    "type": "int",
+                    "default_value": "5"
                 }
             }
         }"""
 
     def execute(self, data):
-        #Message(str(sys.version), title = "Version").show()
+        global debug
+        global debug_layer
+        debug = self.getSettingValueByKey("DebugEnabled")
+        debug_layer = self.getSettingValueByKey("DebugLayer")
+        
+        debug_layer_count = 0
         try:
-
             for i in range(2,len(data)-1):
                 layer = Layer(data[i])
 
-                new_instructions = 'M98 P"' + self.getSettingValueByKey("ToSub") + '";VESUVIUS SUBTRACTIVE\n'
-
                 buffer_distance = self.getSettingValueByKey("SubtractivePerimeter")
+                add_instructions,sub_instructions = layer.expand(buffer_distance)
 
-                n = layer.expand(buffer_distance)
+                #Logger.log("d", "Sub instructions (execute!) = " + str(sub_instructions))
 
-                for ins in n:
-                    new_instructions += ins
+                new_instructions = ""
 
-                new_instructions += 'M98 P"' + self.getSettingValueByKey("ToAdd") + ';END OF VESUVIUS SUBTRACTIVE\n'
+                if self.getSettingValueByKey("AdditiveEnabled"):
+                    new_instructions += "G1 F"+str(self.getSettingValueByKey("AdditiveFeedrate"))+";VESUVIUS EXTRA WALL\n"
+
+                    for ins in add_instructions:
+                        new_instructions += ins
+
+                    new_instructions += ";END OF VESUVIUS EXTRA WALL\n"
+
+                if self.getSettingValueByKey("SubtractiveEnabled"):
+                    new_instructions += "T1;VESUVIUS SUBTRACTIVE\n"
+                    new_instructions += "G1 F"+str(self.getSettingValueByKey("SubtractiveFeedrate"))+"\n"                
+
+                    for ins in sub_instructions:
+                        new_instructions += ins
+
+                    new_instructions += "T0;END OF VESUVIUS SUBTRACTIVE\n"
 
                 data[i] += new_instructions
 
         except Exception as e:
             Message(traceback.format_exc(), title = "Exception").show()
+
+        Message("ASMBL Processing Complete", title = "Status").show()
 
         return data
